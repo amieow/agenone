32a070a449efbd2253177c6b33d6f3b223c34625
amieow <109803246+amieow@users.noreply.github.com>
2023-04-27 15:28:33 +0700

    Add files via upload

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 export async function GET(request: Request) {
   return new Response('Hello, Next.js!')
 }
+=======
+export async function GET(request: Request) {
+  return new Response('Hello, Next.js!')
+}
+>>>>>>> d0dab96 (first commit)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 'use client';
 
 import Service from "@/component/services";
@@ -12,3 +13,20 @@ const Page = () => {
 };
 
 export default Page;
+=======
+
+import ClientComponent from "./component/ClientComponent";
+import Hero from "./component/hero";
+import Service from "./component/services";
+
+const Page = () => {
+  return (
+    <>
+    <Hero/>
+    </>
+
+  );
+};
+
+export default Page;
+>>>>>>> d0dab96 (first commit)
