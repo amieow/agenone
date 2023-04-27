fd29485ada436f7ec4c9a207c0eff8d17575513a
amieow <109803246+amieow@users.noreply.github.com>
2023-04-27 21:03:40 +0700

    Update route.ts

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
+
 export async function GET(request: Request) {
   return new Response('Hello, Next.js!')
 }
-=======
-export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
-}
->>>>>>> d0dab96 (first commit)
